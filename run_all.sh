6280a8d23b58ba28a9625f4241303c0ad7fea07b
Mike Lockhart <m.p.lockhart@gmail.com>
2021-06-18 16:21:31 +0100

    Commit out files

--- run_all.sh
+++ run_all.sh
@@ -1,9 +1,9 @@
-for f in `ls cd44/*.pdb`; do grep -i ATOM $f > ${f:0:-4}_atoms.pdb ; done
-for f in `ls tsg6/*.pdb`; do grep -i ATOM $f > ${f:0:-4}_atoms.pdb ; done
+#for f in `ls cd44/*.pdb`; do grep -i ATOM $f > ${f:0:-4}_atoms.pdb ; done
+#for f in `ls tsg6/*.pdb`; do grep -i ATOM $f > ${f:0:-4}_atoms.pdb ; done
 
-for f in `ls cd44/*_atoms.pdb`; do ./hdock tsg6/*_atoms.pdb $f -out ${f:0:-4}.out ; done
+#for f in `ls cd44/*_atoms.pdb`; do ./hdock tsg6/*_atoms.pdb $f -out ${f:0:-4}.out ; done
 for f in `ls *.out`; do mkdir ${f:0:-4}; done
 for f in `ls *.out`; do cp $f ${f:0:-4}; done
 
-for f in `ls *.out`; do cd ${f:0:-4}; do ../createpl $f top100.pdb -nmax 100 -complex -models; rename s/model/${f:0:-4}/g *.pdb; done
+for f in `ls *.out`; do cd ${f:0:-4}; ../createpl *.out top100.pdb -nmax 100 -complex -models; rename s/model/${f:0:-4}/g *.pdb; done
 
